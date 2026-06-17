b80140a28be14e84bc0dbe7db6a176a0b90197dc
Shripada Mahato <https://github.com/settings/emails>
2026-06-17 09:02:22 +0530

    mean calculation program

---
+++ calculate_mean.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class calculate_mean {
+    public static void main(String[] args) {
+        /*1st number */
+       Scanner num = new Scanner(System.in);
+       System.out.println("enter the 1st num:- ");
+       int num1 = num.nextInt();
+       System.out.println(num1);
+
+        /*2nd number */
+
+       Scanner num2 = new Scanner(System.in);
+       System.out.println("enter the 2nd num:- ");
+       int num3 = num.nextInt();
+       System.out.println(num3);
+
+        /*3rd number */
+
+       Scanner num5 = new Scanner(System.in);
+       System.out.println("enter the 3rd num:- ");
+       int num6 = num.nextInt();
+       System.out.println(num6);
+
+       float average = (num1 + num3 + num6) / 3;
+       System.out.println("Mean:- " + average);
+    }
+}
