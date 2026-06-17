3d4e0e68b83150623e296b494fbce6e32cf57c27
Shripada Mahato <https://github.com/settings/emails>
2026-06-17 08:36:10 +0530

    addition program

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 15;
+        float sum = num1 + num2;
+        System.out.println("Addition of "+sum);
+    }
+}
